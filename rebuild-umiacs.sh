7fa4445b0da4d245f7f3c83987c0f1dff4c0063c
Wu, Ke <wuke@cs.umd.edu>
2012-04-25 17:15:31 -0400

    script for rebuilding cdec under UMIACS environment

---
+++ rebuild-umiacs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+make distclean
+/usr/local/stow/autoconf-2.65/bin/autoreconf -if && \
+    ./configure --with-boost=/fs/clip-software/user-supported/boost/boost-current --disable-gtest && \
+    make -kj8
