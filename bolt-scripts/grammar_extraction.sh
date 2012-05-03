07c586a105f2bdab74c1f48cfc52fb389ef5361c
Wu, Ke <wuke@cs.umd.edu>
2012-05-03 10:59:34 -0400

    BOLT script for grammar extraction; still needs working timing code

---
+++ bolt-scripts/grammar_extraction.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# Extracts grammar for files from the standard input
+
+usage () {
+    cat <<EOF
+$0
+  -g: grammar output dir
+  -c: config file (extract.ini)
+  -j: number of parallel workers
+
+  -h: show this message
+EOF
+    exit 1
+}
+
+cpus=10				# num of parallel workers
+grammar=""			# where to put grammar output
+config=""			# extract.ini
+
+while getopts g:j:c:h o; do
+    case "$o" in
+	g)
+	    grammar="$OPTARG";;
+	j)
+	    cpus="$OPTARG";;
+	c)
+	    config="$OPTARG";;
+	h)
+	    usage;;
+    esac
+done
+shift $(($OPTIND-1))
+
+if [ -z "$grammar" ]; then
+    echo "grammar output dir is required."
+    usage
+fi
+
+if [ -z "$config" ]; then
+    echo "extract.ini is required."
+    usage
+fi
+
+CDEC="$(readlink -f $(dirname $0)/..)"
+echo "using cdec: $CDEC"
+echo "config file: $config"
+echo "using $j workers"
+echo "writing to $grammar"
+
+"$CDEC/sa-extract/escape-testset.pl" | "$CDEC/dpmert/parallelize.pl" -e "$grammar/error" -j "$cpus" --use-fork -- "$CDEC/sa-extract/extractor.py" -c "$config" -x "$grammar/grammar" 2>&1 > /dev/null
