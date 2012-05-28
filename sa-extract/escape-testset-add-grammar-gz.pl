5a73e1572402f2b0c6419eac00f67bfbb6830d6b
Wu, Ke <wuke@cs.umd.edu>
2012-05-28 14:52:12 -0400

    escape with grammar.gz

---
+++ sa-extract/escape-testset-add-grammar-gz.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use utf8;
+use strict;
+
+binmode(STDIN,":utf8");
+binmode(STDOUT,":utf8");
+
+my @fh = ();
+push @fh, \*STDIN;
+
+my $id = -1;
+for my $f (@fh) {
+  while(<$f>) {
+    chomp;
+    die "Empty line in test set" if /^\s*$/;
+    die "Please remove <seg> tags from input:\n$_" if /^\s*<seg/i;
+    $id++;
+    # print "<seg id=\"$id\"> $_ </seg>\n";
+    if (scalar(@ARGV)>0) {
+      print "<seg id=\"$id\" grammar=\"$ARGV[0].$id.gz\"> $_ </seg>\n";
+    } else {
+      print "<seg id=\"$id\"> $_ </seg>\n";
+    }
+  }
+}
+
