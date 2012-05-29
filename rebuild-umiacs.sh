389f9de6d23cb1691a3c5488d4da80c7a70dda96
Wu, Ke <wuke@cs.umd.edu>
2012-05-29 18:50:51 -0400

    also make sa-extract (on umiacs)

--- rebuild-umiacs.sh
+++ rebuild-umiacs.sh
@@ -3,3 +3,4 @@ make distclean
 /usr/local/stow/autoconf-2.65/bin/autoreconf -if && \
     ./configure --with-boost=/fs/clip-software/user-supported/boost/boost-current --disable-gtest && \
     make -kj8
+cd sa-extract && make -f Makefile.umiacs
