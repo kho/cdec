037fc371080a81c75b1c41d2f14438f58f5cb22c
Wu, Ke <wuke@cs.umd.edu>
2012-07-17 11:49:23 -0400

    Customizable output dir

--- tarball.sh
+++ tarball.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+usage () {
+    echo "Usage: $0 output_dir"
+    exit 1
+}
+
+if [ -z "$1" ]; then
+    usage
+fi
 COMMIT=`git log --pretty=format:'%h' -n 1`
-OUTPUT="cdec-$COMMIT.tar.gz"
+OUTPUT="$1/cdec-$COMMIT.tar.gz"
 git archive --prefix=cdec-$COMMIT/ --format=tar HEAD | gzip - > "$OUTPUT"
 echo "Saved `readlink -f "$OUTPUT"`"
